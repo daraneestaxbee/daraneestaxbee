2b3a15a4ffb1f61fc7e16ee6a0e5a96ad12f4640
daraneestaxbee <99231546+daraneestaxbee@users.noreply.github.com>
2022-06-29 18:24:06 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="shortcut icon" href="assets/img/logo/favicon.png" />
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+</head>
+<body class="antialiased">
+    <?php include 'header.php' ?>
+
+    <div class="container xl:w-[1280px]  mx-auto  mt-8 px-2 ">
+             <div class="text-center">
+                <h2 class="text-2xl font-bold md:text-3xl mb-2 uppercase">we are<span class="text-emerald-500 ml-2">taxbee</span></h2>
+                <h3 class="text-lg tracking-wider font-bold mb-5 md:text-xl capitalize text-emerald-800">Providing You With the Most Actionable Tax Solution</h3>
+             </div>
+             <div class="my-12 border-0 rounded-xl bg-emerald-200 leading-loose p-3 font-medium mx-3">
+        <!-- <div class="flex flex-col lg:flex-row my-12 border-0 rounded-xl bg-emerald-200"> -->
+            <!-- <div class="flex flex-col self-center  p-6 space-y-6 md:pl-[5rem] lg:w-1/2">
+                    <div class="flex flex-row  space-x-10  content-center">
+                        <div class="w-[100px] h-[100px] overflow-hidden rounded-full"><img src="./assets/img/team/yuvaraj.jpg"/></div>
+                        <div class="self-center justify-self-center">
+                           <h4 class="text-base tracking-wide font-medium md:text-lg capitalize text-emerald-800">R. Yuvaraj B.E</h4>
+                           <p class="text-stone-800 tracking-[1px]">Managing Director</p> 
+                        </div>
+                    </div>
+                    <div class="flex flex-row space-x-10  content-center">
+                        <div class="w-[100px] h-[100px] overflow-hidden rounded-full"><img src="./assets/img/team/madesh.jpg"/></div>
+                        <div class="self-center">
+                          <h4 class="text-base tracking-wide font-medium md:text-lg capitalize text-emerald-800">K. Madesh MSc (Edin), ACCA</h4>
+                          <p class="text-stone-800 tracking-[1px]">Executive Director</p> 
+                        </div>
+                    </div>
+                    <div class="flex flex-row space-x-10  content-center">
+                        <div class="w-[100px] h-[100px] overflow-hidden rounded-full"><img src="./assets/img/team/gokul.jpg"/></div>
+                        <div class="self-center">
+                          <h4 class="text-base tracking-wide font-medium md:text-lg capitalize text-emerald-800">Gokula Kannan MBA</h4>
+                          <p class="text-stone-800 tracking-[1px]">Project Finance Consultant</p> 
+                        </div>
+                    </div>
+            </div>  -->
+
+            <!-- <div class="lg:w-1/2 p-5"> -->
+              <p class="max-w-full text-gray-900 text-base md:text-lg text-left self-center justify-center mb-2">At Taxbee, we bring deep industry and functional proficiency and a range of perceptions that question the status quo and catalyst change. We work in an exclusively concerted model across the firm and throughout all levels of the client organization, fuelled by the goal of serving our clients flourish and enabling them to make better business decisions.</p>   
+              <p class="max-w-full text-gray-900  text-base md:text-lg text-left self-center justify-center">We work with a commitment to excellence and integrity across the organization, bringing a passion for client success and a purpose to serve and improve. In a world where rapid change and unprecedented disruption are the new normal, we inspire confidence and empower change in all we do. Taxbee is an innovative platform in Business Consulting to provide end-to-end support to the client, through a cloud-based business model with minimum lead time.</p>
+            <!-- </div> -->
+
+        </div>
+    </div>
+
+    <?php include 'footer.php' ?>
+    <script src="./assets/js/script.js"></script>
+</body>
+</html>
